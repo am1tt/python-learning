d026bb9cb1e1ee90bf53abe9c4611681ec285fd4
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-22 11:51:29 +0530

    [ updated and added ] helloworld.py content and indentations.py with its respective content

---
+++ 01-basic-syntax/01-helloworld.py
@@ -0,0 +1,8 @@
+
+
+## hello world ## 
+
+print("Hello, World!")
+
+## use python3 /basic-syntax/01-helloworld.py to run this file ##
+
---
+++ 01-basic-syntax/02-indentation.py
@@ -0,0 +1,14 @@
+
+## Indentation in python ## 
+
+if True:
+    print("this is true")
+else: 
+    print("this is false")
+
+
+## no need to add ; at the end of line ##
+
+## true will execute first ##
+
+## : is used to indentent further in case of condiations and others ##
