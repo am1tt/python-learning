ca5e5e8be5fb1b33aa488624559be61af64b1d29
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-22 17:04:14 +0530

    [ added ] non repetative expressions | setting changing variables data types py

---
+++ 02-variables-and-data-types/03-non-repetative-expressions.py
@@ -0,0 +1,12 @@
+
+## intiating the use of variables here for non repetative expressions ## 
+
+holder_ex1 = 2.4
+
+holder_ex2 = 10.5
+
+total = holder_ex1 * 4.5 + holder_ex2 - holder_ex1 + 5
+
+print(total)
+
+# here we utlized the variables holder_ex1 and ex2 to create a non repetative experession ##
---
+++ 02-variables-and-data-types/05-setting-changing-variables-data-type.py
@@ -0,0 +1,20 @@
+
+
+## here we will see how to change the data type of a variable ##
+
+
+name = "am1t"
+
+
+print(name,type(name)) # this will return value and the type of variable it is ##
+
+
+# lets change a type of a variable ## 
+
+age = 21.5 # this is a float type of variable ## 
+
+age = int(age) #this will change the float to int ## 
+
+print(age , type(age)) 
+
+## this will give us 21 <class 'int'> ## as output
