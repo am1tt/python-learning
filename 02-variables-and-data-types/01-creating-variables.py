cc897e041de7d6c2688b946f636229fd58d5383d
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-22 12:24:58 +0530

    [ added ] creating-variables py

---
+++ 02-variables-and-data-types/01-creating-variables.py
@@ -0,0 +1,27 @@
+
+
+## in python one does not need to add a variable type before creating a variable , like in c++ or other languages ##
+
+## we do something like ## 
+
+am1t_age = 21 
+
+## however , the value that we added in what manner makes it a type ##
+
+## for eg we add value in "" it will be a string ## | ## if we add number it will be an int ## | or if we add decimal it will be a float ##
+
+
+## more examples ##
+
+string_v = "am1t" #string 
+
+number_v = 21 #int
+
+decimal_v = 21.2 #float
+
+list_v = [1,2,3] ; list_string = ["am1t","is","cool"] #list for string
+
+list_ordinal = {1:"first",2:"second"}
+
+print(list_v,list_string)
+print(list_ordinal)
