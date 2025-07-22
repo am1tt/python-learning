9ae9a98fbd0576a8b23675fb501a794342976b91
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-22 17:17:14 +0530

    [ added ] data-storage-variables | dynamic-type-variable

---
+++ 02-variables-and-data-types/06-data-storage-variables.py
@@ -0,0 +1,14 @@
+
+
+## here we will see how data storage variables work ## 
+## data storage variables can be used to store chunks of data in a single variable ## 
+
+
+my_contacts = [
+    ("am1t","2425425925","am1t@gmail.com"),
+    ("john","2940353556","john@gmail.com")
+]
+
+print(my_contacts)
+
+## this will give us list of tuples ## 
---
+++ 02-variables-and-data-types/07-dynamic-type-variable.py
@@ -0,0 +1,14 @@
+
+
+## python consist of dynamic types variable , where it changes the type of variable based on the value assigned to it ## 
+
+## eg 
+
+a = "am1t"
+
+
+a = 21.78
+
+print(a)
+
+## this will give us 21.78 as output as the value of a is changed to float now ## s
