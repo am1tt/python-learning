a0d88cbf498967ec5d9f3512fb349371e3a45828
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-22 17:04:51 +0530

    [ added ] expressions py | temporary variables and manual swapping performed

---
+++ 02-variables-and-data-types/02-expressions.py
@@ -0,0 +1,17 @@
+
+
+## here we will learn about experessions in python ##
+
+
+ex = 1 * 2.1 * 8
+
+print(ex)
+
+ex2 = 1 + 2 - 5 * 2.5 
+
+print(ex2)
+
+## to build expressions we use operators like + , - , * , / , ** , // , % ## 
+
+## here in ex we used * (multiplication) operator to build an expression ##
+
---
+++ 02-variables-and-data-types/04-temporary-variables-(manual-swapping).py
@@ -0,0 +1,18 @@
+
+## here we will see how to create temporary variables #
+
+## this temporary variable will help us swap values of two variables such tha a and b ## 
+
+a = 10
+b = 20
+
+
+temp = a
+a = b
+b = temp
+
+print(a,b)
+
+## here temp took the value of a and then assigned the value of b to a then finally temp(a) was assigned to b ##
+
+## this is manaul swapping of values ##
