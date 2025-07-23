34a8a36a2ff89ed7e38fd53f0642ca5aa351ccc3
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 17:36:32 +0530

    [ added ] integer-conversion py | explicit type conversion py

---
+++ 02-basic-block/01-type-casting/01-interger-conversion.py
@@ -0,0 +1,15 @@
+
+
+## converting integer to float ##
+
+integer_num = 777
+
+float_num = 7.77
+
+
+converted_num = integer_num + float_num
+
+print(f"this is new number {converted_num}")
+
+print(type(converted_num))
+
---
+++ 02-basic-block/01-type-casting/02-explicit-type-conversion.py
@@ -0,0 +1,18 @@
+
+## here i will try explicit type conversion ## 
+
+## in explicit conversion , user convert the data type of an object to required data type## 
+
+## TL;DR : using built in functions : int(),float(),str() etc 
+
+
+number_is_string = "777"
+
+
+number_is_string = int(number_is_string)
+
+
+
+print(f"{number_is_string} is : ",type(number_is_string))
+
+## this conversion is also called : (type casting)  #
