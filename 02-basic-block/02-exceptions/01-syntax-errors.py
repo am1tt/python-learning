c2f69fb8a6a1cc464ed1dbf4a9627ba601e8715a
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 18:23:07 +0530

    [ added ] syntax-error py | mathematical exception py

---
+++ 02-basic-block/02-exceptions/01-syntax-errors.py
@@ -0,0 +1,11 @@
+
+
+## here im learning about a common syntax error : 
+
+
+while True print('foo')
+
+
+# this gives an file<stdin> error
+
+# to fix - we need to add  : here to create a further indendation 
---
+++ 02-basic-block/02-exceptions/02-mathematical-exception.py
@@ -0,0 +1,4 @@
+
+10 *  (1 / 0)
+
+## well you cannot divide by zero 
