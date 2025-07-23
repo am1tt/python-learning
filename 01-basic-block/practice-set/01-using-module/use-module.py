979dc5f756a183584ab8c331525e8c52e79e8ab1
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 13:38:10 +0530

    [ created & added ]practice-sets |  user-module.py

---
+++ 01-basic-block/practice-set/01-using-module/use-module.py
@@ -0,0 +1,11 @@
+
+
+## question : install an external module and use it TO perform an operation based on your interest##
+
+import random
+
+random_number = random.randint(1,10)
+
+print(f"printing random number between 1 and 10 : {random_number}")
+
+## here i used random module and create instance of random number that will help us print random number ##
