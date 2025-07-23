8abb49fe9829c86cf8dd7117b0cba6433aa3a1a2
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 14:51:53 +0530

    [ added ] poem py

---
+++ 01-basic-block/practice-set-1/00-print-poem/poem.py
@@ -0,0 +1,35 @@
+
+my_poem = """
+[Verse 1]
+In the dark , We come out and play
+We are its children, And were here to stay
+Running through , Hungry for strays
+No invitation, take me away
+Im not cruel, But thats still what you see
+Club to club, Come see this city with me
+Hungry for life, Without your pity
+I dont want it, But you give it
+
+Still cant say she wont start up
+Still cant say she wont start up a fight
+You go city
+Cause in the city of life she cant she cant wait
+
+[Verse 2]
+In the darkness, A killer awaits
+To **** a life, And the lies you make
+You do another, So this death can live
+Just keep on dancing, To the movie your in
+The smell of your sweat, Just lures me in
+Your heartbeat, Does sing to me
+Running feet, Beats my blood
+My ghost inside you, Soon will be
+
+"""
+
+
+print(my_poem)
+
+## decided to utilzed literals i learned about ## 
+
+## am1tt : solved
