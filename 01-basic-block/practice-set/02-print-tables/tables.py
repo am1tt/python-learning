e244a7ae78fdd5b4b22cbd0187a1b69f6d48a0da
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 13:38:36 +0530

    [ added ] tables py | print dir py

---
+++ 01-basic-block/practice-set/02-print-tables/tables.py
@@ -0,0 +1,15 @@
+
+
+## print the table of 5 ##
+
+
+n = int(input("enter the number to print table : "))
+
+
+for i in range(1,n):
+    print(f"{n} * {i} = {n*i}") 
+
+## here n refers to the size added : 5 
+## i referes to the range of numbers (iterations) : 1,2,3
+## hence multiplying size and the iteration will cause to print the numbers ## 
+
---
+++ 01-basic-block/practice-set/03-print-directory/print-dir.py
@@ -0,0 +1,10 @@
+
+## write a python program to print the directory to print th contents of the directory using OS module ##
+
+import os 
+
+current_directory = os.getcwd() 
+
+print(f"current directory : {current_directory}") 
+
+## @am1t : this prints the current_directory 
