3487f6897f8fdc5436fd93c3ead4ff55006654aa
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 15:07:31 +0530

    [ added ] remainder py | type py

---
+++ 01-basic-block/practice-set-2/02-find-remainder/remainder.py
@@ -0,0 +1,14 @@
+
+
+## write a program to find the remainder when a number is divided by z ##
+
+num = int(input("enter the number : "))
+
+z = int(input("enter the divisor : "))
+
+res = num % z 
+
+print(f"remainder when {num} is divided by {z} is : {res}")
+
+# am1tt : completed the task ## 
+
---
+++ 01-basic-block/practice-set-2/03-type-using-input/type.py
@@ -0,0 +1,17 @@
+
+
+## check the type of variable assigned using input() function ## 
+
+my_input = input()
+
+# and for the int one #
+
+int_input = int(input())
+
+print(type(my_input))
+print(type(int_input))
+
+## am1tt : well string is always "Default for inputs" so if you want to include non strings 
+## am1tt : convert into the desired type you want Eg : int_input 
+
+## am1tt : solved
