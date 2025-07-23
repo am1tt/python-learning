9f0989aff9654a28e07acbf27fd63358109894c3
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-23 15:05:53 +0530

    [ created & added ] practice-set 2 | addition.py

---
+++ 01-basic-block/practice-set-2/01-add-numbers/addition.py
@@ -0,0 +1,14 @@
+
+## write a python program to add two numbers ## 
+
+num = int(input("enter first number : "))
+
+num2 = int(input("enter second number : "))
+
+
+res = num + num2
+
+print(f"sum of {num} and {num2} is : {res}")
+
+
+# @am1tt : i utlized input here , as i have habit of using it # 
