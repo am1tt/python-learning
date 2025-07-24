31510d1c4217dfda6bb24ed304d5701f9f5fa9e8
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-24 17:36:02 +0530

    [ added ] built-in functions py

---
+++ 02-basic-block/03-functions/built-in-functions/01-built-in-functions.py
@@ -0,0 +1,40 @@
+
+## am1tt : well built in functions are somewhat integrated functions that comes with a language ## 
+
+
+## eg ; 
+
+name = input("")
+
+## input is built in func 
+
+x = 2
+
+isinstance(x,(int,float))
+
+## used in previous exception code , checks wether var is int float 
+
+
+
+my_string = "Amit"
+
+print(len(my_string))
+
+## len gives the length of the string
+
+
+x = abs(-23.4)
+print(x)
+
+## Prints the absolute value : abs
+
+
+my_l = list(("a","b","c"))
+
+print(my_l)
+
+
+for i in range(1,6):
+    print(i)
+
+## range is also a built in func 
