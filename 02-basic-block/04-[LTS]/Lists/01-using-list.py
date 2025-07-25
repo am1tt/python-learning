1085fae652ff5ebcf477bf4ab1fd415b14838d23
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-25 12:31:57 +0530

    [ added ] list py

---
+++ 02-basic-block/04-[LTS]/Lists/01-using-list.py
@@ -0,0 +1,11 @@
+
+
+## basically are used to multiple items in a sinlge var 
+
+foo = ["foo1","foo2","foo2"]
+
+print(foo)
+
+## or we can print in loop which is common as well 
+
+for i in foo:print([i])
