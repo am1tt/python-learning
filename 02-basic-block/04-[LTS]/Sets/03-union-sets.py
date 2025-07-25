c044451147e3648b0e41441c0df3a98e693f0e2f
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-25 15:28:24 +0530

    [ added ] intersections | union sets py

---
+++ 02-basic-block/04-[LTS]/Sets/03-union-sets.py
@@ -0,0 +1,8 @@
+
+
+set_a = {1,2,3}
+
+set_b = {4,5,6}
+
+## well it combines it 
+print(set_a.union(set_b))
---
+++ 02-basic-block/04-[LTS]/Sets/04-intersections-sets.py
@@ -0,0 +1,10 @@
+
+
+
+set_a = {1,2,3}
+
+set_b = {4,1,6}
+
+print(set_a.intersection(set_b))
+
+## it gives common element 
