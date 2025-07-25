5df81d57214fb3e7c7ec4914994c44d43ab96e78
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-25 14:13:42 +0530

    [ added ] using two list py

---
+++ 02-basic-block/04-[LTS]/Lists/03-using-two-list.py
@@ -0,0 +1,11 @@
+
+
+original_list = [1,2,3,4,5,6]
+
+square_list = []
+
+for i in original_list:
+    square_list.append(i * i)
+
+print(f"original list : {original_list}")
+print(f"squared list : {square_list}")
