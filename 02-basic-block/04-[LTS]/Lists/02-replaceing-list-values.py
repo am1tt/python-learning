fc0c0709338c11a38fa01adbfa290963103d7790
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-25 14:07:58 +0530

    [ added ] replacing list values py

---
+++ 02-basic-block/04-[LTS]/Lists/02-replaceing-list-values.py
@@ -0,0 +1,16 @@
+
+
+## replacing values in list 
+
+li = [23,35,12,3]
+## it all depends on the index you chooose 
+
+li[1] = 99
+
+print(li)
+
+## we can also use append here 
+
+li.append(97)
+
+print(li)
