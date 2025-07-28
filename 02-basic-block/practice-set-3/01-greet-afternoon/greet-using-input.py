63f5511c4b6394aabe141798ce71b8ae899ebbc6
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-28 14:46:02 +0530

    [ added ] greet-using-input | letter template py

---
+++ 02-basic-block/practice-set-3/01-greet-afternoon/greet-using-input.py
@@ -0,0 +1,8 @@
+
+# Q : Write a python program to display a user entered name followed by Good Afternoon using input() functions # 
+
+name = input("name : ")
+
+print(f"Good Afternoon {name}")
+
+# am1tt : solved #
---
+++ 02-basic-block/practice-set-3/02-fill-template/letter-template.py
@@ -0,0 +1,17 @@
+
+
+## Write a program to fill in a letter template given below with name and date # 
+
+name = input("name : ")
+date = "6/7/25"
+
+
+letter = f'''
+Dear {name}
+    You are Selected ! {date}
+'''
+
+
+print(letter)
+
+# am1tt : solved # 
