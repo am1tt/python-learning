b23ff727ad667aaf1bfbe6f22c416e33aeb155ed
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-30 17:05:00 +0530

    [ added ] change-val-of-list-inside-set py

---
+++ 02-basic-block/practice-set-5/09-change-value-of-list/change-val-of-list-inside-set.py
@@ -0,0 +1,24 @@
+
+
+## Q : can you change the values inside a list which is contained in set s ? ## 
+
+s = {8,7,12,"am1tt",[1,2]}
+
+
+## sets allow hashables or immutables objects 
+## list is mutable though , so its unhashable 
+
+## therefore we cannot change neither print it as it will result in typerror of list ## 
+
+## to print 
+
+s_print = {8,7,12,"am1tt",(1,2)}
+
+print(s_print)
+
+## we cannot change the value though ## 
+
+
+## solved : am1tt 
+
+
