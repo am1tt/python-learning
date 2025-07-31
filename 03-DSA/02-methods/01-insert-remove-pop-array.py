ed250fe04fff054f297b07f739fbe454631ccff7
Amit Sakpal <eflatmajor811@gmail.com>
2025-07-31 18:32:39 +0530

    [ added ] insert-remove-pop-array py

---
+++ 03-DSA/02-methods/01-insert-remove-pop-array.py
@@ -0,0 +1,23 @@
+
+
+
+from array import array 
+
+
+arr = array('i',[])
+
+
+arr.insert(0,35)
+
+arr.insert(1,45) ## takes 2 params btw , 1 for index and other for val # 
+
+
+
+arr.remove(45)
+
+
+arr.pop()
+
+print(arr)
+
+
