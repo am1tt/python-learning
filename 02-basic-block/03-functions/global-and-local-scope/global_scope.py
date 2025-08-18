4510f2d763ad768e4cb2c0c16ed455df60be512d
Amit Sakpal <eflatmajor811@gmail.com>
2025-08-18 15:44:40 +0530

    [ added ] global_scope py

---
+++ 02-basic-block/03-functions/global-and-local-scope/global_scope.py
@@ -0,0 +1,14 @@
+
+
+greet = "hello"
+
+
+def sayHello(): 
+    greet = "hi"
+
+    return greet
+
+
+print(sayHello())
+
+## as greet variable is outside the function it makes it global variable easily accessible inside functions 
