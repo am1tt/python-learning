34efc472558333071780aee6247d4d4f302eb10a
Amit Sakpal <eflatmajor811@gmail.com>
2025-08-18 15:43:45 +0530

    [ added ] local_scope py

---
+++ 02-basic-block/03-functions/global-and-local-scope/local_scope.py
@@ -0,0 +1,13 @@
+
+
+def foo(num):
+    res = num * num
+    return res
+
+
+foo_square = foo(4)
+
+
+print(f"square : {foo_square}")
+
+## well here inside the function , res is a local scope or local variable
