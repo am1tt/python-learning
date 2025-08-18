05b86ac61a26a916ff61332db4cf32aa7b5ab5ce
Amit Sakpal <eflatmajor811@gmail.com>
2025-08-18 17:43:50 +0530

    [ added ] main_function py

---
+++ 02-basic-block/03-functions/main-function/main_function.py
@@ -0,0 +1,12 @@
+
+
+def main(): 
+    print("this is main")
+
+
+if __name__ == "__main__":
+    main()
+
+## main is used to organize code 
+## ensures that specific code only runs when file is exec directly
+
