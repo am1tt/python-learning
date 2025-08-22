5bb2723d2a8c2a36db749e0ad71e884481eca616
Amit Sakpal <eflatmajor811@gmail.com>
2025-08-22 15:28:39 +0530

    [ added ] public-members py

---
+++ 04-OOPS/03-Encapsulation/public-members/public-members.py
@@ -0,0 +1,21 @@
+
+
+## from 1 of the access modifiers 
+
+## Public modifier 
+
+class Person: 
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("am1tt",21)
+
+print(p1.name)
+print(p1.age)
+
+
+## no restriction here 
+
+
